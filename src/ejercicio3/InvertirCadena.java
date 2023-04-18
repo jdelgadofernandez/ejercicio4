bcbd3af742885d099b24521abbed9c39471357aa
am291092 <am291092@gmail.com>
2023-04-18 10:53:29 +0200

    InvertirCadena

---
+++ src/ejercicio3/InvertirCadena.java
@@ -0,0 +1,18 @@
+package ejercicio3;
+
+public class InvertirCadena {
+
+	public String invertir(String cadena) {
+		String cadenaInvertida="";
+		
+		for (int i=cadena.length();i>=0;i--) {
+			
+			cadenaInvertida=cadenaInvertida+cadena.charAt(i);
+			
+		}
+		
+		return cadenaInvertida;
+	}
+	
+	
+}
